2709e17f6f502b4ea0946173d9aa47aa6511ce7f
Dmitrii Selivanov <dmitry.selivanov@castle.co>
2016-09-09 19:33:22 +0400

    Removed unnecessary `index` option on `belongs_to` migration method

--- db/migrate/20160908180036_create_railway_stations_routes.rb
+++ db/migrate/20160908180036_create_railway_stations_routes.rb
@@ -1,8 +1,8 @@
 class CreateRailwayStationsRoutes < ActiveRecord::Migration
   def change
     create_table :railway_stations_routes do |t|
-      t.belongs_to :route, index: true
-      t.belongs_to :railway_station, index: true
+      t.belongs_to :route
+      t.belongs_to :railway_station
     end
   end
 end
