3eb23e54522667db713b27133d52d5a150fa5361
Dmitrii Selivanov <dmitry.selivanov@castle.co>
2016-10-06 13:54:01 +0200

    Fixed typo in migration

--- db/migrate/20161006100022_add_indexes_to_railway_stations_routes.rb
+++ db/migrate/20161006100022_add_indexes_to_railway_stations_routes.rb
@@ -1,6 +1,6 @@
-class AddIndexesToRailwayStationsRoutes < ActiveRecord::Migration
+class AddIndexToRailwayStationsRoutes < ActiveRecord::Migration
   def change
     add_index :railway_stations_routes, :route_id
-    add_index :railway_stations_routes, :railway_station_idTi
+    add_index :railway_stations_routes, :railway_station_id
   end
 end
